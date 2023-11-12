c9ed758ade6f36cd1bcd5a970a0c479333c89e04
Cosmos Joseph <josephcosmos70@gmail.com>
2023-11-12 05:54:55 +0100

    modified:   blog/__pycache__/admin.cpython-311.pyc

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
+
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted 
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                            {'post':post,
+                             'form': form,
+                             'comment': comment})
+
+
+
 
 class PostListView(ListView):
     """
@@ -23,9 +47,15 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
                             
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
     return render (request,
                    'blog/post/detail.html',
-                   {'post': post})
+                   {'post': post,
+                    'comments':comments,
+                    'form':form})
 
 
 def post_share (request, post_id):
